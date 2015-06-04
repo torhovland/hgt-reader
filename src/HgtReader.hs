966ef45a8162e2e62f2b6554fac195656293f97b
Tor Hovland <tor@hovland.co>
2015-06-04 21:51:40 +0200

    Can now read real elevations from hgt files.

--- src/HgtReader.hs
+++ src/HgtReader.hs
@@ -1,43 +1,66 @@
 module HgtReader where
 
 import qualified Data.ByteString as S
-import Control.Monad
 import qualified Data.Set as Set
 import Text.Printf
 
 type Coords = (Double, Double)
-type ElevatedPoint = (Coords, Maybe Integer)
+type ElevatedPoint = (Coords, Maybe Int)
 
 mkUniq :: Ord a => [a] -> [a]
 mkUniq = Set.toList . Set.fromList
 
+truncLatLon :: RealFrac a => a -> Int
+truncLatLon l = abs (floor l) :: Int
+
 quadrant :: Coords -> String
 quadrant (latitude, longitude) = 
-	printf "%s%02d%s%03d" x truncLat y truncLon
+	printf "%s%02d%s%03d" y truncLat x truncLon
 	where
-		truncLat = (abs (floor latitude)) :: Int
-		truncLon = (abs (floor longitude)) :: Int
-		x = (if latitude > 0 then "N" else "S")
-		y = (if longitude > 0 then "E" else "W")
+		truncLat = truncLatLon latitude
+		truncLon = truncLatLon longitude
+		x = if longitude > 0 then "E" else "W"
+		y = if latitude > 0 then "N" else "S"
 
 quadrants :: [Coords] -> [String]
 quadrants = mkUniq . map quadrant
 
 prepare :: [Coords] -> IO [ElevatedPoint]
-prepare l = do
-	return $ map (\x -> (x, Nothing)) l
+prepare l = return $ map (\x -> (x, Nothing)) l
 
-elevationInQuadrant :: S.ByteString -> ElevatedPoint -> ElevatedPoint
-elevationInQuadrant ele ((latitude, longitude), Nothing) = ((latitude, longitude), Just 124)
-elevationInQuadrant _ point = point
+elevationAtCellIndex :: S.ByteString -> Int -> Int
+elevationAtCellIndex ele n = 
+	firstByte * 256 + secondByte
+	where
+		firstByte = fromIntegral (S.head . S.drop (n*2) $ ele) :: Int
+		secondByte = fromIntegral (S.head . S.drop (n*2+1) $ ele) :: Int
 
-elevationsInQuadrant :: String -> String -> IO [ElevatedPoint] -> IO [ElevatedPoint]
-elevationsInQuadrant path q points = do
+elevationInQuadrant :: S.ByteString -> Int -> ElevatedPoint -> ElevatedPoint
+elevationInQuadrant ele quadrantSize (coords, Nothing) = 
+	(coords, Just (elevationAtCellIndex ele index))
+	where
+		index = cellIndex quadrantSize coords
+elevationInQuadrant _ _ point = point
+
+elevationsInQuadrant :: String -> String -> Int -> IO [ElevatedPoint] -> IO [ElevatedPoint]
+elevationsInQuadrant path q quadrantSize points = do
 	let file = path ++ "/" ++ q ++ ".hgt"
 	elevationData <- S.readFile file
 	p <- points
-	return $ map (elevationInQuadrant elevationData) p
+	return $ map (elevationInQuadrant elevationData quadrantSize) p
 
-elevations :: String -> [Coords] -> IO [ElevatedPoint]
-elevations path l = foldl (\acc x -> elevationsInQuadrant path x acc) (prepare l) (quadrants l)
+cellIndex :: Int -> Coords -> Int
+cellIndex quadrantSize (latitude, longitude) = 
+	round ((1.0 - y) * (q - 1)) * quadrantSize +
+	round (x * (q - 1))
+	where
+		q = fromIntegral quadrantSize
+		truncLat = fromIntegral . truncLatLon $ latitude
+		truncLon = fromIntegral . truncLatLon $ longitude
+		x = longitude - truncLon
+		y = latitude - truncLat
+
+elevations :: String -> Int -> [Coords] -> IO [ElevatedPoint]
+elevations path quadrantSize l = 
+	foldl (\acc x -> elevationsInQuadrant path x quadrantSize acc) (prepare l) (quadrants l)
 
--- tests/Spec.hs
+++ tests/Spec.hs
@@ -2,7 +2,7 @@ import Test.Hspec
 import HgtReader
 
 main :: IO ()
-main = hspec $ do
+main = hspec $
   describe "HgtReader" $ do
     it "determines the quadrant a coordinate point belongs in" $ do
       quadrant (62.3, 10.1) `shouldBe` "N62E010"
@@ -10,21 +10,35 @@ main = hspec $ do
       quadrant (62.3, -10.1) `shouldBe` "N62W011"
       quadrant (-62.3, -10.1) `shouldBe` "S63W011"
 
-    it "determines the quadrants for a list of coordinates" $ do
+    it "determines the quadrants for a list of coordinates" $
       quadrants
 	[(62, 10.1), 
 	 (62.1, 10), 
 	 (63.0, 10)]
 	`shouldBe` ["N62E010", "N63E010"]
 
+    it "determines the cell index of a coordinate point inside a quadrant" $ do
+      cellIndex 3 (62.9, 10.0) `shouldBe` 0
+      cellIndex 3 (62.9, 10.9) `shouldBe` 2
+      cellIndex 3 (62.5, 10.5) `shouldBe` 4
+      cellIndex 3 (62.0, 10.0) `shouldBe` 6
+      cellIndex 3 (62.0, 10.9) `shouldBe` 8
+      cellIndex 3 (62.24, 10.0) `shouldBe` 6
+      cellIndex 3 (62.26, 10.0) `shouldBe` 3
+      cellIndex 3601 (62.0, 10.99999) `shouldBe` 3601 * 3601 - 1
+
     it "determines elevations for a list of coordinates" $ do
-      calc <- elevations "elevation"
-	[(62, 10.1), 
-	 (62.1, 10), 
-	 (63.0, 10)]
+      calc <- elevations "elevation" 3601
+	[(62.0, 10.0), 
+	 (62.0, 10.9999), 
+	 (62.9999, 10.0),
+ 	 (62.64706079, 10.33255505),
+	 (62.64164653, 10.36175275)]
       calc
 	`shouldBe` 
-	[((62.0, 10.1), Just 133), 
-	 ((62.1, 10.0), Just 13), 
-	 ((63.0, 10.0), Just 1)]
+	[((62.0, 10.0), Just 800), 
+	 ((62.0, 10.9999), Just 895), 
+	 ((62.9999, 10.0), Just 539),
+	 ((62.64706079, 10.33255505), Just 888),
+	 ((62.64164653, 10.36175275), Just 1240)]
 
